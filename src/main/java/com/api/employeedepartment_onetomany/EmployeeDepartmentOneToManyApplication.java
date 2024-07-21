4dfc82f4e5cf609fb72b251ef9540246ebf45ed1
Ednilson Chiziane <eddybruno43@gmail.com>
2024-07-21 12:24:52 +0200

    feat():CRUD implementation again to 2

--- src/main/java/com/api/employeedepartment_onetomany/EmployeeDepartmentOneToManyApplication.java
+++ src/main/java/com/api/employeedepartment_onetomany/EmployeeDepartmentOneToManyApplication.java
@@ -3,11 +3,23 @@ package com.api.employeedepartment_onetomany;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class EmployeeDepartmentOneToManyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeeDepartmentOneToManyApplication.class, args);
+        SpringApplication app = new SpringApplication(EmployeeDepartmentOneToManyApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
+        app.run(args);
+    }
+
+    private static String getPort() {
+        String port = System.getenv("PORT");
+        if (port == null || port.isEmpty()) {
+            return "8080";
+        }
+        return port;
     }
 
 }
